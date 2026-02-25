3794e85239ff23ecad543d3d7d75f233b5bb5f71
José <jose.carpio@gmail.com>
2026-02-25 11:33:12 +0100

    Add documentation for num_elem predicate
    Add a comment block explaining the num_elem predicate.

---
+++ num_elem.pl
@@ -0,0 +1,11 @@
+/*
+
+ num_elem(+Lista, -Resultado)
+   es cierto si Resultado unifica con el
+   número de elementos de Lista.
+   
+Principio de inducción
+1. num_elem([], ).
+2. num_elem(n-1) -> num_elem(n)
+
+*/
