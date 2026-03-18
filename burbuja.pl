66fa39438acca9efe4c51aa195877b223d6c39d5
José <jose.carpio@gmail.com>
2026-03-18 10:02:26 +0100

    Implement bubble sort algorithm in burbuja.pl

--- burbuja.pl
+++ burbuja.pl
@@ -5,7 +5,16 @@
 %   de menor a mayor.
 %-----------------------------------------------------
 
+ordena_burbuja(Lista, Lista):- ordenada(Lista).
 
+ordena_burbuja(Lista, R):- \+ ordenada(Lista), append(L1, [Elem1, Elem2|L2], Lista),
+  Elem1 > Elem2,
+  append(L1, [Elem2, Elem1|L2], ListaN), 
+  ordena_burbuja(ListaN, R).
+
+ordena_burbuja(Lista, R):- \+ ordenada(Lista), append(_, [Elem1, Elem2|_], Lista),
+  Elem1 =< Elem2,
+  ordena_burbuja(Lista, R).
 
 
 /*
@@ -15,7 +24,8 @@ ordenada(+Lista)
 
 */
 
-
 ordenada([]).
 ordenada([_]).
 ordenada([Cab1, Cab2|Resto]):- Cab1 =< Cab2, ordenada([Cab2|Resto]).
+
+
