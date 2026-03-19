2361233d3ae7a73cbc53519f27cf4c432e74e630
José <jose.carpio@gmail.com>
2026-03-19 09:55:15 +0100

    Add quicksort implementation in Prolog
    Implement quicksort algorithm with divide and ordena_quick predicates.

--- quicksort.pl
+++ quicksort.pl
@@ -7,7 +7,21 @@
 %   mayores que Elem.
 %------------------------------------------------------
 
+divide(_, [], [], []).
+divide(Elem, [Cab|Resto], [Cab|RMe], RMa):- Cab =< Elem, divide(Elem, Resto, RMe, RMa).
+divide(Elem, [Cab|Resto], RMe, [Cab|RMa]):- Cab > Elem, divide(Elem, Resto, RMe, RMa).
 
+%---------------------------------------------------
+% ordena_quick(+Lista, -ListaR).
+%es cierto cuando ListaR unifica con una lista que
+%contiene los mismos elementos que Lista ordenados
+%de menor a mayor.
+%------------------------------------------------
+
+ordena_quick([], []).
+ordena_quick([Pivote|Resto], R):- divide(Pivote, Resto, Me, Ma), 
+                                   ordena_quick(Me, RMe), ordena_quick(Ma, RMa),
+								   append(RMe, [Pivote|RMa], R).
 
 
 
